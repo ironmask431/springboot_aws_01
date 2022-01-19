db31641a47c325550fa855ef022f9b775635206b
ironmask431 <ironmask431@gmail.com>
2022-01-19 21:41:10 +0900

    2022.01.19 커밋 (HelloResponseDto)

---
+++ src/test/java/com/jojodu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,27 @@
+package com.jojodu.book.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+        //assertThat : assertj 라는 테스트검증 라이브러리의 검증메소드
+        //검증하고싶은 대상을 인자로 받음
+        //isEqualTo : assertj 의 동등 비교 메소드
+    }
+
+}
