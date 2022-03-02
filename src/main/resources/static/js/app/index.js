da32c70334a46fbc99ffbadd1d52384205d452f0
ironmask431 <ironmask431@gmail.com>
2022-03-03 06:57:58 +0900

    1. 작성자이름을 로그인한 사용자의 name으로 등록되도록 수정.(완) 2. 제목,내용 미입력시 alert 노출(완)

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -12,12 +12,15 @@ var index = {
         });
     },
     save : function(){
+        var _this = this;
         var data = {
             title:$('#title').val(),
             author:$('#author').val(),
             content:$('#content').val()
         };
-
+        if(_this.validation_check(data) == false){
+            return;
+        }
         $.ajax({
             type:'POST'
             ,url:'/api/v1/posts'
@@ -34,43 +37,57 @@ var index = {
         })
     },
     update : function(){
-            var id  = $('#id').val();
-            var data = {
-                title:$('#title').val(),
-                content:$('#content').val()
-            };
-            $.ajax({
-                type:'PUT'
-                ,url:'/api/v1/posts/'+id
-                ,dataType:'json'
-                ,contentType:'application/json; charset=utf-8'
-                ,data:JSON.stringify(data)
-            })
-            .done(function(){
-                alert('수정되었습니다.');
-                location.href='/';
-            })
-            .fail(function(error){
-                alert(JSON.stringify(error));
-            })
-        },
-        delete : function(){
-            var id  = $('#id').val();
-
-            $.ajax({
-                type:'DELETE'
-                ,url:'/api/v1/posts/'+id
-                ,dataType:'json'
-                ,contentType:'application/json; charset=utf-8'
-            })
-            .done(function(){
-                alert('삭제되었습니다.');
-                location.href='/';
-            })
-            .fail(function(error){
-                alert(JSON.stringify(error));
-            })
+        var _this = this;
+        var id  = $('#id').val();
+        var data = {
+            title:$('#title').val(),
+            content:$('#content').val()
+        };
+        if(_this.validation_check(data) == false){
+            return;
         }
+        $.ajax({
+            type:'PUT'
+            ,url:'/api/v1/posts/'+id
+            ,dataType:'json'
+            ,contentType:'application/json; charset=utf-8'
+            ,data:JSON.stringify(data)
+        })
+        .done(function(){
+            alert('수정되었습니다.');
+            location.href='/';
+        })
+        .fail(function(error){
+            alert(JSON.stringify(error));
+        })
+    },
+    delete : function(){
+        var id  = $('#id').val();
+        $.ajax({
+            type:'DELETE'
+            ,url:'/api/v1/posts/'+id
+            ,dataType:'json'
+            ,contentType:'application/json; charset=utf-8'
+        })
+        .done(function(){
+            alert('삭제되었습니다.');
+            location.href='/';
+        })
+        .fail(function(error){
+            alert(JSON.stringify(error));
+        })
+    },
+    validation_check : function (data){
+        if(data.title == ''){
+            alert('제목을 입력하세요');
+            return false;
+        }
+        if(data.content == ''){
+            alert('내용을 입력하세요');
+            return false;
+        }
+        return true;
+    }
 } //index
 
 index.init();
