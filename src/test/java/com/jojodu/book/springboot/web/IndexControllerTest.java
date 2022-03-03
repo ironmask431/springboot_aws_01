5acbd7ef1086b1f9bfcce9640ba8327b8ddb3f20
ironmask431 <ironmask431@gmail.com>
2022-03-04 08:42:24 +0900

    텍스트수정

--- src/test/java/com/jojodu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojodu/book/springboot/web/IndexControllerTest.java
@@ -21,6 +21,6 @@ public class IndexControllerTest {
         String body = restTemplate.getForObject("/",String.class);
         //then
         //html 전체를 확인할 필요는 없으니 일부내용만 있는지 확인함.
-        assertThat(body).contains("스프링부트로 시작하는 웹서비스");
+        assertThat(body).contains("SpringBoot + AWS 웹서비스");
     }
 }
